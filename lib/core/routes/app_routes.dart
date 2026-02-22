5007cf69d3ae88aa1fe4e32520fe0c6a8045ccee
omarhassan168 <oh569678@gmail.com>
2026-02-22 23:15:04 +0200

    family members UI + mock data

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -10,11 +10,9 @@ import '../../features/Auth/presentation/view/terms_and_conditions/screens/terms
 import '../../features/Onboarding/presentation/screens/onboarding_screen.dart';
 import '../../features/Onboarding/presentation/screens/pick_language_screen.dart';
 import '../../features/articles/presentation/view/screens/articles_bookmarks_screen.dart';
-import '../../features/articles/presentation/view/screens/articles_search_screen.dart';
 import '../../features/favorite_doctors/presentation/view/screens/favorite_doctors_screen.dart';
 import '../../features/layout/presentation/view/main_layout_view.dart';
 import '../../features/app_settings/presentation/view/screens/settings_screen.dart';
-import '../../features/profile/presentation/view/screens/add_family_member_screen.dart';
 import '../../features/profile/presentation/view/screens/family_members_screen.dart';
 
 class AppRoutes {
